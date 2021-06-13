bf83eb7b2f4f7bbdddc7b265bbaa9b3f531ee9e5
benjaminmerchin <benjaminmerchin@gmail.com>
2021-06-13 20:36:43 +0200

    oui

---
+++ rendu/mariadb/createdb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+service mysql start
+
+RUN echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mysql -u root --skip-password
+RUN echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" | mysql -u root --skip-password
+RUN echo "UPDATE mysql.user set plugin='mysql_native_password' WHERE user='root';" | mysql -u root --skip-password
+RUN echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
+
+service mysql restart
