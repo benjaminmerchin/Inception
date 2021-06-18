a3f55a07eda6434181e6586c247b68f39ff6b9b5
benjaminmerchin <benjaminmerchin@gmail.com>
2021-06-18 20:21:13 +0200

    oui

--- Makefile
+++ Makefile
@@ -3,12 +3,22 @@ all: up
 up:
 	docker-compose -f ./srcs/docker-compose.yml up -d --build 
 
+upv:
+	docker-compose -f ./srcs/docker-compose.yml up --build 
+
 down:
 	docker-compose -f ./srcs/docker-compose.yml down
 #--remove-orphans
+downv:
+	docker-compose -f ./srcs/docker-compose.yml down -v
 
 network:
 	docker network ls
 
 ps:
 	docker-compose -f ./srcs/docker-compose.yml ps
+
+logs:
+	@docker logs wordpress
+	@docker logs nginx
+	@docker logs mariadb
--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -26,10 +26,10 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -78,7 +78,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
